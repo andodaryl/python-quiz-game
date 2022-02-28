25e361b2f58ceba217baac934a47230c0cb61be1
Daryl Ando <ando.daryl@hotmail.com>
2022-02-28 15:45:17 +0000

    Update data + var names & add startup behaviour

--- display_API.py
+++ display_API.py
@@ -6,8 +6,22 @@ API for display related behaviour.
 from pprint import pprint
 
 # Data
-display_config = {
-  welcome_message: 'Hello, are you ready for a maths quiz?'
+input_type = {
+  "yes" : 'Y',
+  "no" : 'N',
+}
+
+display = {
+  "welcome_message" : "Hello, are you ready for a maths quiz?",
+  "yes_or_no": f"Yes = {input_type['yes']} \n No = {input_type['no']}'"
 }
 
 # Behaviour
+print(display["welcome_message"])
+
+app_start_request = input(f"Start quiz?\n{display['yes_or_no']}\n")
+
+app_inactive = lambda app_start_request : app_start_request != display["input_yes"]
+
+if app_inactive:
+  quit()
