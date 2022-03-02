e0b623401c526d9f8226493a0bceceab7e50cbe0
Daryl Ando <ando.daryl@hotmail.com>
2022-03-02 01:01:09 +0000

    Add game over message & remove redudant input_mark

--- display_API.py
+++ display_API.py
@@ -5,20 +5,25 @@ API for display related behaviour.
 # Imports
 from choice_API import choice_type
 from question_API import is_answer_correct
+from score_API import max_reward
+from score_API import show_score
 
 # Data
 display = {
   "input_mark": ">>> ",
   "welcome_message" : "Hello, are you ready for a maths quiz?",
   "goodbye_message": "Goodbye, see you next time!",
+  "game_over_message": f"The quiz is over, your score is {show_score()} out of {max_reward}!",
   "yes_or_no": f"Yes = {choice_type['YES']} No = {choice_type['NO']}",
   "yes_to_continue": f"Enter '{choice_type['YES']}' to continue",
 }
 
 # Behaviour
-welcome_user = lambda: print(f"{display['welcome_message']} {display['input_mark']}")
+welcome_user = lambda: print(f"{display['welcome_message']}")
 
-farewell_user = lambda: print(f"{display['goodbye_message']} {display['input_mark']}")
+farewell_user = lambda: print(f"{display['goodbye_message']}")
+
+game_over = lambda: print(f"{display['game_over_message']}")
 
 request_app_start = lambda: input(f"{display['yes_to_continue']} {display['input_mark']}")
 
