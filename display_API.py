2600d11ff14e6ce48489e9617d925b6b70895da1
Daryl Ando <ando.daryl@hotmail.com>
2022-03-02 00:31:32 +0000

    Update input_mark & convert messages to f-strings

--- display_API.py
+++ display_API.py
@@ -8,7 +8,7 @@ from question_API import is_answer_correct
 
 # Data
 display = {
-  "input_mark": " >>> ",
+  "input_mark": ">>> ",
   "welcome_message" : "Hello, are you ready for a maths quiz?",
   "goodbye_message": "Goodbye, see you next time!",
   "yes_or_no": f"Yes = {choice_type['YES']} No = {choice_type['NO']}",
@@ -16,10 +16,10 @@ display = {
 }
 
 # Behaviour
-welcome_user = lambda: print(display["welcome_message"] + display["input_mark"])
+welcome_user = lambda: print(f"{display['welcome_message']} {display['input_mark']}")
 
-farewell_user = lambda: print(display["goodbye_message"] + display["input_mark"])
+farewell_user = lambda: print(f"{display['goodbye_message']} {display['input_mark']}")
 
-request_app_start = lambda: input(display['yes_to_continue'] + display["input_mark"])
+request_app_start = lambda: input(f"{display['yes_to_continue']} {display['input_mark']}")
 
-request_user_answer = lambda question_number=0, current_question="a + b = ?": input(f"{str(question_number)}: {current_question}{display['input_mark']}")
+request_user_answer = lambda question_number=0, current_question="a + b = ?": input(f"{str(question_number)}: {current_question} {display['input_mark']}")
