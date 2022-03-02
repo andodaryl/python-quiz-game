434a289e92f2bf55d2120fbd3f99493b78b553cc
Daryl Ando <ando.daryl@hotmail.com>
2022-03-02 01:53:22 +0000

    Fix question generator

--- question_API.py
+++ question_API.py
@@ -3,6 +3,7 @@ Module for question generator and related behaviour.
 '''
 # Import
 from random import randint
+from random import choice as get_random_item
 
 # Behaviour
 max_questions = 10
@@ -14,8 +15,8 @@ question_type = (
   lambda a=get_random_number(), b=get_random_number(): (f"{a} x {b} = ?", a * b), # multiply
   lambda a=get_random_number(), b=get_random_number(): (f"{a*b} / {a} = ?", b), # divide
 )
-get_random_question = lambda: question_type[randint(0, len(question_type) - 1)]
-generate_question = lambda get_generator=get_random_question(): get_generator()
+
+generate_question = lambda: get_random_item(question_type)()
 
 get_questions = lambda total=max_questions: tuple([generate_question() for question_number in range(total)])
 
